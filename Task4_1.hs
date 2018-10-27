4680a5f1c33fca09484e4fd2886ecd8dc2253b19
Ira <Irina_Revkina@epam.com>
2018-10-27 13:07:43 +0300

    Create task4_1

--- Task4_1.hs
+++ Task4_1.hs
@@ -5,3 +5,15 @@ module Task4_1 where
 data FunMonad a = FunMonad { fun :: String -> a }
 
 -- реализуйте классы `Functor`, `Applicative` и `Monad` для типа `FunMonad`
+
+instance Functor FunMonad where
+    fmap f (FunMonad a) = FunMonad (f . a)
+
+instance Applicative FunMonad where
+    pure a = FunMonad (\x -> a)
+    FunMonad a <*> FunMonad b = FunMonad $ \x -> (a x) (b x)
+
+instance Monad FunMonad where
+    return a  = FunMonad $ \x -> a
+    FunMonad a >>= b = FunMonad (\x -> fun (b $ a x) x)
+    fail = error
