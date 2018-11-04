74a7b549311aa7fbc322947b578a0711e9dc65a1
Ira <Irina_Revkina@epam.com>
2018-11-04 14:13:19 +0300

    Create task 6

--- Task6.hs
+++ Task6.hs
@@ -2,11 +2,36 @@ module Task6 where
 
 import Todo(todo)
 
-data LinkedTree a = ChangeMe
+data LinkedTree a = EmptyTree
+    | Node a (LinkedTree a) (LinkedTree a) (LinkedTree a) deriving (Eq, Show)
 
-find :: LinkedTree a -> a -> Bool
-find = todo
-insert :: LinkedTree a -> a -> LinkedTree a
-insert = todo
-remove :: LinkedTree a -> a -> LinkedTree a
-remove = todo
+find :: (Ord a) => LinkedTree a -> a -> Bool
+find EmptyTree _ = False
+find (Node value left right _) a
+    | value == a = True
+    | value > a = find left a
+    | value < a = find right a
+
+insert :: (Ord a) => LinkedTree a -> a -> LinkedTree a
+insert EmptyTree a = Node a EmptyTree EmptyTree EmptyTree
+insert (Node value EmptyTree right parent) a
+    | a < value = newParent
+        where newParent = Node value newNode right parent
+              newNode = Node a EmptyTree EmptyTree newParent
+insert (Node value left EmptyTree parent) a
+    | a > value = newParent
+        where newParent = Node value left newNode parent
+              newNode = Node a EmptyTree EmptyTree newParent
+insert x@(Node value left right parent) a
+    | a < value = Node value (insert left a) right parent
+    | a > value = Node value left (insert right a) parent
+    | otherwise = x
+
+remove :: (Ord a) => LinkedTree a -> a -> LinkedTree a
+remove EmptyTree _ = EmptyTree
+remove (Node value left right parent) a
+    | a < value = Node value (remove left a) right parent
+    | a > value = Node value left (remove right a) parent
+    | otherwise = join left right
+        where join x EmptyTree = x
+              join x (Node value left right parent) = Node value (join left x) right parent
